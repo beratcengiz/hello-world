b5ef852da49eebd4d93bec19913ffe23164fffb8
berat <berat.cengiz@armagroupholding.com>
2024-01-24 17:55:24 +0300

    d

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/router'
-import { createApp } from 'vue';
-import PrimeVue from 'primevue/config';
-import 'primevue/resources/themes/lara-light-green/theme.css'
 const app = createApp(App)
 app.use(router)
-app.use(PrimeVue)
 app.mount('#app')
